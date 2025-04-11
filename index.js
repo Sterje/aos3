23af838ae6d06dda751c1807a000e95cd1281623
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-04-11 23:54:32 +0200

    updates

---
+++ index.js
@@ -0,0 +1,44 @@
+ScrollReveal().reveal(".cardimg", {
+    origin: "bottom",
+    distance: "100px",
+    duration: 3000,
+    delay: 100,
+  }); // Initialize ScrollReveal
+
+  ScrollReveal().reveal(".cardtext", {
+    origin: "bottom",
+    distance: "100px",
+    duration: 3000,
+    delay: 400,
+  }); // Initialize ScrollReveal
+
+  ScrollReveal().reveal(".cardimg2", {
+    origin: "bottom",
+    distance: "100px",
+    duration: 3000,
+    delay: 800,
+  }); // Initialize ScrollReveal
+
+  ScrollReveal().reveal(".div-container", {
+    origin: "left",
+    distance: "300px",
+    duration: 3000,
+    delay: 200,
+  }); // Initialize ScrollReveal
+
+  
+
+
+ScrollReveal().reveal(".up", {
+    origin: "bottom",
+    distance: "400px",
+    duration: 1500,
+    delay: 300,
+  }); // Initialize ScrollReveal
+
+ScrollReveal().reveal(".left", {
+    origin: "left",
+    distance: "400px",
+    duration: 1500,
+    delay: 300,
+  }); // Initialize ScrollReveal
